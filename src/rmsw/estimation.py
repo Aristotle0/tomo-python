6fa38a160872d4fafff6e563f76bb2bb1256cb3f
Lei Pan <panlei.life@gmail.com>
2014-11-18 20:41:46 +0800

    Add fictitious freqential bands

--- src/rmsw/estimation.py
+++ src/rmsw/estimation.py
@@ -9,7 +9,7 @@ def get_index(ks, kf, N, Ns, Nf, Ks, Kf):
     N is the size of array T
     """
     indx = []
-    for f in range(kf-1, kf-1+Nf-2*Kf):
+    for f in range(kf-1, kf-1+Nf):
         for s in range(ks-1, ks-1+Ns-2*Ks):
             indx.append(f*Ns+s)
     indx2 = [x+N/2 for x in indx]
@@ -32,17 +32,28 @@ def select_subarray(T, ks, kf, Ns, Nf, Ks, Kf):
     T_sub_H = T_sub.conj().T
     return np.outer(T_sub, T_sub_H)
 
+def extend_T(T, Ns, Nf, Nc, Kf):
+    """ Extend T and add the ficticius frequential bands
+
+    """
+    tmp = T.reshape(Nf*Nc, Ns)
+    col_pad = np.zeros((Nf*Nc, Kf))
+    tmp = np.hstack((col_pad, tmp, col_pad))
+    return tmp.flatten()
+
+
 def emsm(T, Ns, Nf, Nc, Ks, Kf):
     """ Estimated multicomponent spectral matrix
 
     Its dimension is (M x M) with M = (Ns - 2Ks)*(Nf - 2Kf)*Nc
     """
-    M = (Ns-2*Ks)*(Nf-2*Kf)*Nc
+    M = (Ns-2*Ks)*Nf*Nc
     ret = np.zeros((M, M), dtype=np.complex)
+    T_extend = extend_T(T, Ns, Nf, Nc, Kf)
     for kf in range(1, 2*Kf+2):
         for ks in range(1, 2*Ks+2):
             # print('--%02i--%02i' % (ks, kf))
-            ret += select_subarray(T, ks, kf, Ns, Nf, Ks, Kf)
+            ret += select_subarray(T_extend, ks, kf, Ns, Nf, Ks, Kf)
     return ret
 
 
@@ -55,14 +66,15 @@ if __name__ == '__main__':
     Ks = 72; Kf = 3; Nc = 2
 
     from time import clock
-    M = (Ns-2*Ks)*(Nf-2*Kf)*Nc
+    M = (Ns-2*Ks)*Nf*Nc
     x = np.zeros((M, M), dtype=np.complex)
+    T = extend_T(T, Ns, Nf, Nc, Kf)
     start = clock()
     # x = emsm(T, Ns, Nf, 2, Ks, Kf)
     for j in range(5):
         for i in range(5):
             x += select_subarray(T, i+1, j+1, Ns, Nf, Ks, Kf)
     end = clock()
-    print("cost time: %.4f" % ((end-start)))
+    print("cost time: %.4f" % ((end-start)/25))
 
 
