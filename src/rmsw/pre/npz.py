e715c7e0a3bdffc244fad3d7324d2e725ea4f35c
Lei Pan <panlei.life@gmail.com>
2014-11-18 08:48:47 +0800

    precondition for data

---
+++ src/rmsw/pre/npz.py
@@ -0,0 +1,31 @@
+# Preconditions for test data Vx.npz and Vz.npz.
+# Their structure is (time, nsta)
+
+import numpy as np
+
+def fft_trans(X):
+    """ Perform a FFT along time axis
+    """
+    X_new = np.fft.fft(X, axis=0)
+    return X_new.flatten()
+
+def read_npz(path):
+    """ Read the npz file Vx.npz and Vz.npz, then pretreat them.
+    """
+    xfile = np.load(path + 'Vx.npz')
+    zfile = np.load(path + 'Vz.npz')
+    terms = ['obs', 'syn']
+    ret = {}
+    for term in terms:
+        x = fft_trans(xfile['Vx_'+term])
+        z = fft_trans(zfile['Vz_'+term])
+        tmp_co = np.concatenate((x, z))
+        ret[term] = tmp_co
+    return ret
+
+
+if __name__ == '__main__':
+    Td = read_npz('../../../data/')
+    print(Td['obs'].shape, Td['obs'].dtype)
+
+
